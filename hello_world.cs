084e9e3deae2f441073d89a336922fed21a1c2b3
Nick <NickyDaB@gmail.com>
2024-03-13 19:35:52 -0400

    Create hello_world.cs

---
+++ hello_world.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class hello_world : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Nick!");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
